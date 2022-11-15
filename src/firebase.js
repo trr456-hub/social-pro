c5b90851e000416a6ebea55d05802bfe704163a6
gorhf9397 <trr456@naver.com>
2022-11-15 14:56:41 +0900

    react-dom firebase import및 version setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwIJHKKSpwVZBWuFRbIj8Hxo4NH4c9u84",
+  authDomain: "social-pro-89a4d.firebaseapp.com",
+  projectId: "social-pro-89a4d",
+  storageBucket: "social-pro-89a4d.appspot.com",
+  messagingSenderId: "224511454855",
+  appId: "1:224511454855:web:ad537d5ca2ab819b3e9222"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
