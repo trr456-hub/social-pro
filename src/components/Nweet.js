094d37c3444cf9f77c6e0ca3accd694ef29bdebd
seokgeun <trr456@naver.com>
2022-11-20 21:08:43 +0900

    delete,update기능 추가

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,15 +1,66 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>삭제</button>
-        <button>수정</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하겠습니까?");
+    if (ok) {
+      // delete를 가지는 조건문
+      // console.log(ok);
+      await deleteDoc(NweetTextRef);
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (e) => {
+    const ok = window.confirm("수정하겠습니까?");
+    e.preventDefault();
+    if (ok) {
+      //console.log(nweetObj, newNweet);
+      await updateDoc(NweetTextRef, {
+        text: newNweet,
+      });
+    }
+    setEditing(false);
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="수정할 코멘트를 작성해 주세요."
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="수정" />
+          </form>
+          <button onClick={toggleEditing}>취소</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>삭제</button>
+              <button onClick={toggleEditing}>수정</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Nweet;
