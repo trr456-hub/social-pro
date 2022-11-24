fef471e54f361d1ef13ecb070dc79957207d544f
gorhf9397 <trr456@naver.com>
2022-11-24 15:03:50 +0900

    URL  을 변수에 넣어서 업데이트 오브젝트 형태로 addDOC추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,13 @@
 import Nweet from "components/Nweet";
 import { dbService, storageService } from "fbase";
 import { v4 as uuidv4 } from "uuid";
-import { ref, uploadString } from "firebase/storage";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import {
   collection,
   onSnapshot,
   query,
   orderBy,
+  addDoc,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
@@ -14,7 +15,7 @@ const Home = ({ userObj }) => {
   //console.log(userObj); //ID값 조회
   const [nweet, setNweet] = useState(""); // 글작성 tweet을 가지는 useState 작성
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState();
+  const [attachment, setAttachment] = useState("");
   // const getNweets = async () => {
   //   const dbNweets = await getDocs(collection(dbService, "nweets"));
   //   dbNweets.forEach((document) => { // forEach 로 작성 하면 rerender 해줘야함(새로고침)
@@ -45,16 +46,34 @@ const Home = ({ userObj }) => {
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
-    // const fileRef = ref(storageService, `${userObj}/${v4()}`); // npm install uuid 설치 : 식별자 랜덤생성
-    const fileRef = ref(storageService, `${userObj}/${uuidv4()}`); // db 업로드
-    const response = await uploadString(fileRef, attachment, "data_url");
-    console.log(response);
-    // await addDoc(collection(dbService, "nweets"), {
-    //   text: nweet,
-    //   createdAt: Date.now(),
-    //   creatorId: userObj.uid,
-    // });
-    // setNweet("");
+    let attachmentUrl = "";
+    // const attachmentRef = ref(storageService, `${userObj}/${v4()}`); // npm install uuid 설치 : 식별자 랜덤생성
+    //이미지 첨부하지 않고 텍스트만 올리고 싶을 때도 있기 때문에 attachment가 있을때만 아래 코드 실행
+    //이미지 첨부하지 않은 경우엔 attachmentUrl=""이 된다.
+    if (attachment !== "") {
+      //파일 경로 참조 만들기
+      const attachmentRef = ref(storageService, `${userObj}/${uuidv4()}`); // db 업로드
+      //storage 참조 경로로 파일 업로드 하기
+      const response = await uploadString(
+        attachmentRef,
+        attachment,
+        "data_url"
+      );
+      //storage 참조 경로에 있는 파일의 URL을 다운로드해서 attachmentUrl 변수에 넣어서 업데이트
+      attachmentUrl = await getDownloadURL(response.ref);
+    }
+    const nweetObj = {
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    };
+    //트윗하기 누르면 nweetObj 형태로 새로운 document 생성하여 nweets 콜렉션에 넣기
+    await addDoc(collection(dbService, "nweets"), nweetObj);
+    //state 비워서 form 비우기
+    setNweet("");
+    //파일 미리보기 img src 비워주기
+    setAttachment("");
   };
   const onChange = (event) => {
     const {
@@ -82,7 +101,7 @@ const Home = ({ userObj }) => {
   // console.log(nweet);
   // console.log(nweets);
   const onClearPhoto = () => {
-    setAttachment(null);
+    setAttachment("");
   };
   return (
     <div>
