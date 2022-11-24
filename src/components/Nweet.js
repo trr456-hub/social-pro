3e1a587d74e75b5483f2dc30e00013e62f1e6b43
gorhf9397 <trr456@naver.com>
2022-11-24 15:44:07 +0900

    fb v9 문법으로 사진의 경로를 변수에 담고 deleteObject 사용 삭제기능 구현

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,17 +1,22 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState } from "react";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNewNweet] = useState(nweetObj.text);
   const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+  const photoDel = ref(storageService, nweetObj.attachmentUrl);
+  // 사진의 url경로를 변수에 담음
   const onDeleteClick = async () => {
     const ok = window.confirm("삭제하겠습니까?");
     if (ok) {
       // delete를 가지는 조건문
       // console.log(ok);
       await deleteDoc(NweetTextRef);
+      await deleteObject(photoDel);
+      // deleteObject 명령어를 사용해 변수에 담은 경로의 사진을 삭제
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
