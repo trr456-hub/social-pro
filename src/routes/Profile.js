f9db484189bfa9f3c0d3396b248195bf4ee26b67
gorhf9397 <trr456@naver.com>
2022-11-24 17:06:18 +0900

    userId 와 동일한 creatorID를 mypage에 전달 getDocs로 결과 가져옴

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
-export default () => {
+export default (userObj) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/"); // 로그아웃 된 후 다시 홈으로 돌아갈수있게
   };
+  // nweets 얻는 function 생성
+  const getMyNweets = async () => {
+    // nweets 불러오기
+    // dbService의 컬렉션 중 "nweets" Docs에서 userObj의 uid와 동일한 creatorID를 가진 모든 문서를 내림차순으로 가져오는 쿼리(요청) 생성
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", `${userObj.uid}`),
+      orderBy("createdAt", "desc")
+    );
+    // getDocs 메소드로 쿼리 결과값 가지고 오기
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+  // nweets 얻는 function 생성
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>로그아웃</button>
