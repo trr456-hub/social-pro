7650b0f42a2180eb9e55e5af57b46df6b2c6e5f1
gorhf9397 <trr456@naver.com>
2022-12-03 21:15:45 +0900

    회원가입form modal처리 시도 아직 미구현 회원가입 온클릭 window.open구현

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { authService } from "fbase";
+import AuthWindow from "./AuthWindow";
 
 const AuthForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState("");
+
   const onChange = (e) => {
     const {
       target: { name, value }, // target 안에 name, value값을 넣어줌
@@ -20,19 +21,18 @@ const AuthForm = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (newAccount) {
-        //계정생성
-        await authService.createUserWithEmailAndPassword(email, password);
-      } else {
-        //로그인
-        await authService.signInWithEmailAndPassword(email, password);
-      }
+      //계정생성
+      //await authService.createUserWithEmailAndPassword(email, password);
+      //로그인
+      await authService.signInWithEmailAndPassword(email, password);
       //console.log(data);
     } catch (error) {
       setError(error.message);
     }
   };
-  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const setWindow = () => {
+    window.open(`${AuthWindow}`);
+  };
   return (
     <>
       <form onSubmit={onSubmit} className="container">
@@ -54,15 +54,11 @@ const AuthForm = () => {
           className="authInput"
           onChange={onChange}
         />
-        <input
-          type="submit"
-          className="authInput authSubmit"
-          value={newAccount ? "계정생성" : "로그인"}
-        />
+        <input type="submit" className="authInput authSubmit" value="로그인" />
         {error && <span className="authError">{error}</span>}
       </form>
-      <span onClick={toggleAccount} className="authSwitch">
-        {newAccount ? "로그인" : "계정생성"}
+      <span onClick={setWindow} className="authSwitch">
+        회원가입
       </span>
     </>
   );
--- src/components/AuthWindow.js
+++ src/components/AuthWindow.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { authService } from "fbase";
+import React, { useState } from "react";
 
 const AuthWindow = () => {
-  return;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onSubmit = async (e) => {
+    try {
+      //회원가입
+      await authService.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      // 에러창 도출
+      setError(error.message);
+    }
+  };
+  return (
+    <form>
+      <input
+        name="email"
+        type="email"
+        placeholder="Email.."
+        required
+        value={email}
+        className="authInput"
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password.."
+        required
+        value={password}
+        className="authInput"
+      />
+    </form>
+  );
 };
 
 export default AuthWindow;
