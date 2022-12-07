19b7acc83a46fb825af061e4cb0761d72680e990
gorhf9397 <trr456@naver.com>
2022-12-07 17:14:35 +0900

    모달창 화면 외부 클릭하면 모달창 종료 구현중 useRef오류 버그픽스 예정

--- src/components/AuthWindow.js
+++ src/components/AuthWindow.js
@@ -1,11 +1,14 @@
 import { authService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 //import "../modalStyle.css";
 
 const AuthWindow = ({ setModal }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  // 모달 외부 클릭시 끄기 처리
+  // Modal 창을 useRef로 취득
+
   const onChange = (e) => {
     // console.log(e.target.value);
     const {
@@ -31,6 +34,22 @@ const AuthWindow = ({ setModal }) => {
   const closeModal = () => {
     setModal(false);
   };
+  const modalRef = useRef < HTMLDivElement > null;
+  useEffect(() => {
+    const handler = (e) => {
+      // mousedown 이 발생된 영역이 modal창 밖이면 모달창 제거 처리
+      if (modalRef.current || !modalRef.current.contains(e.target)) {
+        setModal(false);
+      }
+    };
+    // eventhandler 등록
+    document.addEventListener("mousedown", handler);
+    return () => {
+      // eventhandler 해제
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [modalRef, setModal]);
+
   return (
     <>
       <div className="modalContainer">
